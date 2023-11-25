8b92e7d1f85298964b83962b946401657eb6f8ec
sgo722 <88089193+sgo722@users.noreply.github.com>
2023-11-25 21:14:16 +0900

    [Silver I] Title: 회의실 배정, Time: 92 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, a,b, last,cnt,ret;
+vector<pair<int,int>> v;
+int main(){
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> a >> b;
+        v.push_back({b,a});
+    }
+
+    sort(v.begin(),v.end());
+    int end = v[0].first;
+    int cnt = 1;
+    for(int i=1; i<v.size(); i++){
+        if(end > v[i].second) continue;
+        else{
+            end = v[i].first;
+            cnt++;
+        }
+    }
+    cout << cnt << "\n";
+    return 0;
+}
