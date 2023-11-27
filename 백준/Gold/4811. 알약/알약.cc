596e0a11b9ec3a270ab90f8ab22a84c2672b66af
sgo722 <88089193+sgo722@users.noreply.github.com>
2023-11-27 21:09:14 +0900

    [Gold V] Title: 알약, Time: 28 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.cc
+++ 백준/Gold/4811. 알약/알약.cc
@@ -3,33 +3,24 @@ using namespace std;
 
 typedef long long ll;
 
-int n, ans;
-ll dp[64][34][34];
-ll go(int day, int hCnt, int wCnt){
-    if(hCnt > n || wCnt > n || hCnt < wCnt){
-        return 0;
-    }
-    if(day == 2 * n){
-        return 1;
-    }
+int n;
+ll dp[34][34];
 
-    ll &ret = dp[day][hCnt][wCnt];
-
-    if(ret != -1){
-        return ret;
-    }
-
-    ret = go(day + 1, hCnt + 1, wCnt) + go(day + 1, hCnt, wCnt + 1);
-
+// 한 알을 꼭 n번만큼 골라내야 한다. n번만큼 골라냈다면 그 경우엔 하나의 문자열이 만들어진것이다.
+ll go(int one, int half){
+    if(half == -1) return 0;
+    if(one == 0) return 1;
+    ll &ret = dp[one][half];
+    if(ret != -1) return ret;
+    ret = go(one-1,half+1) + go(one,half-1);
     return ret;
 }
 int main(){
 
     while(cin >> n){
         memset(dp,-1,sizeof(dp));
-        ans = 0;
         if(n == 0) break;
-        cout << go(1,1,0) << "\n";
+        cout << go(n,0) << endl;
     }
 
     return 0;
