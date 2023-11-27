68f6890f03952fdf3d8ef27bb69670eb85ead22c
sgo722 <88089193+sgo722@users.noreply.github.com>
2023-11-27 13:03:28 +0900

    [Silver IV] Title: 수 찾기, Time: 44 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,36 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, num, m;
+int n,m, num;
 vector<int> v;
-map<int,int> mp;
 
-void go(int start, int end){
-    if(start > end) return;
-    if(start == end){
-        if(mp[v[start]] != 0) cout << 1 << "\n";
-        else cout << 0 << "\n";
-        return;
+
+// 구글링으로 본 코드
+// 1. 이진탐색 로직이 궁금했습니다.
+// 2. 원래 작성한 코드와 시간차이를 확인해보고 싶었습니다.
+void binarySearch(int key){
+    int start = 0;
+    int end = n-1;
+    int mid;
+
+    while(end >= start){
+        mid = (start+end)/2;
+        if(v[mid] == key){
+            cout << 1 << "\n";
+            return;
+        }
+        if(v[mid] > key){
+            end = mid - 1;
+        }
+        else if(v[mid] <= key){
+            start = mid + 1;
+        }
     }
-
-    go(start,(start+end)/2);
-    go((start+end)/2+1,end);
+    cout << 0 << "\n";
 }
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     cin >> n;
     for(int i=0; i<n; i++){
-        cin >> num;
-        mp[num]++;
-    }
-
-
-    cin >> m;
-    for(int j=0; j<m; j++){
         cin >> num;
         v.push_back(num);
     }
 
-    go(0,m-1);
+    sort(v.begin(),v.end());
 
+    cin >> m;
+    for(int i=0; i<m; i++){
+        cin >> num;
+        binarySearch(num);
+    }
     return 0;
 }
