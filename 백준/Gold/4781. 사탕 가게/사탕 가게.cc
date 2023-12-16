be9d14c3bc147c36cf71b5161ec6ec8c450c493c
sgo722 <88089193+sgo722@users.noreply.github.com>
2023-12-16 13:28:59 +0900

    [Gold IV] Title: 사탕 가게, Time: 248 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.cc
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.cc
@@ -1,33 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define INF 2000000004
 
-int n, kcal, myCost, temp, my, cost;
-int dp[10004];
-vector<pair<int,int>> candy;
+int n,m1,m2, kcal, p1,p2;
+int dp[100004];
 
 int main(){
-
-    while(1) {
-        candy.clear();
-        scanf("%d %d.%d", &n, &my, &temp);
-        if(n == 0 && my == 0 && temp == 0) break;
-        myCost = my * 100 + temp;
-        fill(&dp[0], &dp[0]+10004, 0);
+    while(1){
+        scanf("%d %d.%d", &n, &m1, &m2); // 사탕개수, 내가 가진 돈 입력 - 실수형 연산의 한계때문에 정수형으로 입력받기
+        if(n == 0 && m1 == 0 && m2 == 0) break; // 종료조건
+        memset(dp,0,sizeof(dp)); // dp[cost] = kcal => dp는 cost에 해당하는 최대 kcal를 가진다.
+        int m = m1 * 100 + m2; // 내가 가진 돈 계산
         for(int i=0; i<n; i++){
-            scanf("%d %d.%d", &kcal, &cost, &temp);
-            int loadCost = cost * 100 + temp;
-//            cout << kcal << " " << loadCost << "\n";
-            candy.push_back({kcal, loadCost});
-        }
-
-        for(int i=1; i<=myCost; i++){
-            for(int j=0; j<n; j++){
-                if(i-candy[j].second < 0) continue;
-                dp[i] = max(dp[i], dp[i-candy[j].second] + candy[j].first);
+            scanf("%d %d.%d", &kcal, &p1, &p2); // 사탕 칼로리, 사탕가격 입력 - 실수형 연산 한계로 인한 정수형 변환
+            int p = p1 * 100 + p2; // 사탕 가격 계산
+            for(int j = p; j<= m; j++){
+                dp[j] = max(dp[j], dp[j-p]+kcal); // 같은 금액을 지불하는데 더 많은 칼로리를 얻는 경우 확인한다.
             }
         }
-        cout << dp[myCost] << "\n";
+        cout << dp[m] << "\n";
     }
     return 0;
 }
