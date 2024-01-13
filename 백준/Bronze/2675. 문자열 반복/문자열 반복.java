8b1d748cd2c59e6e80d45eace0972390a91be478
sgo722 <88089193+sgo722@users.noreply.github.com>
2024-01-13 13:33:50 +0900

    [Bronze II] Title: 문자열 반복, Time: 112 ms, Memory: 13972 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,22 +1,19 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
 
-// BufferedReader + StringBuilder
+// BufferedReader + StringBuilder + split
 public class Main{
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st;
 		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int a = Integer.parseInt(st.nextToken());
+			String[] str = br.readLine().split(" ");
+			int a = Integer.parseInt(str[0]);
 			StringBuilder sb = new StringBuilder();
-			String s = st.nextToken();
-			for(int j=0; j<s.length(); j++) {
+			for(byte it : str[1].getBytes()) {
 				for(int k=0; k<a; k++) {
-					sb.append(s.charAt(j));
+					sb.append((char)(it));
 				}
 			}
 			System.out.println(sb);
