29268b88ea6df58bf07d64d1ebe8dd2bec1b76b4
sgo722 <88089193+sgo722@users.noreply.github.com>
2024-01-13 09:13:34 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int a = Integer.parseInt(st.nextToken());
+		
+		System.out.println(a-543);
+	}
+}
