fd716a992a2a2745b19565ed7276865cc90d1878
sgo722 <88089193+sgo722@users.noreply.github.com>
2024-01-15 20:35:20 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int main(){
+    cin >> n;
+    for(int i=n; i>0; i--){
+        for(int j=0; j<i; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
