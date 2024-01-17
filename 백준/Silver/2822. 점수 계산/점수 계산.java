15e51a48470126b33b88b48a5ab0127d69003edd
sgo722 <88089193+sgo722@users.noreply.github.com>
2024-01-17 21:27:29 +0900

    [Silver V] Title: 점수 계산, Time: 148 ms, Memory: 15896 KB -BaekjoonHub

--- 백준/Silver/2822. 점수 계산/점수 계산.java
+++ 백준/Silver/2822. 점수 계산/점수 계산.java
@@ -4,39 +4,36 @@ import java.util.*;
 /**
  * 문제 시작 - 20:47
  * 아이디어 - 높은 점수 5개를 골라내고, 처음 인덱스를 출력한다.
- * 아이디어 1 - 원본배열은 두고, 정렬한 배열을 만든다. 그리고 5번 스캔하면서 해당되는 인덱스를 출력한다.
- * -> 시간제한 상 풀리긴 하겠지만, 더 효율적인 코드가 있을 것 같다.
+ * 아이디어2 - 2차원 배열을 이용한다. 
  */
 public class Main{
 	public static void main(String[] args) throws IOException{
 		
-		int[] arr = new int[8];
-		int[] arr2 = new int[8];
-		int[] cnt = new int[5];
+		int[][] arr = new int [8][2];
+		int[] ret = new int [5];
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		int sum = 0;
 		for(int i=0; i<8; i++) {
 			int n = Integer.parseInt(br.readLine());
-			arr[i] = n;
-			arr2[i] = n;
+			arr[i][0] = n;
+			arr[i][1] = i+1;
 		}
-		
-		Arrays.sort(arr);
 		
 		for(int i=0; i<5; i++) {
-			for(int j=0; j<8; j++) {
-				if(arr[7-i] == arr2[j]) {
-					sum += arr2[j];
-					cnt[i] = j+1;
-					break;
-				}
-			}
+			
 		}
-		Arrays.sort(cnt);
+		Arrays.sort(arr,(int[] a, int[] b) -> {
+			return b[0] - a[0];
+		});
+		
+		for(int i=0; i<5; i++) {
+			sum += arr[i][0];
+			ret[i] = arr[i][1];
+		}
+		Arrays.sort(ret);
 		System.out.println(sum);
 		for(int i=0; i<5; i++) {
-			System.out.print(cnt[i] + " ");
+			System.out.print(ret[i] + " ");
 		}
 	}
 }
