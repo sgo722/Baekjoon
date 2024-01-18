defa241f582375bb7a25ab5069428966f72e1814
sgo722 <88089193+sgo722@users.noreply.github.com>
2024-01-19 01:49:44 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,14 +2,17 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 /**
  * 문제 시작 - 01:18
  * 아이디어 - 그래프탐색문제여서 그냥 bfs 구현하면 풀릴 것 같다
  * 다만 C++을 사용하면 vector를 사용하면 금방 맞출수있는 문제라 생각하는데, 자바에서는 어떻게 코드를 짜야할지 잘 모르겠다.
+ * 2. 인접리스트 사용하는법을 몰라서 구글링하고 코드고쳐보기
  */
 public class Main{
-	static int t;
+	static ArrayList<Integer>[] a;
+	static int t, count = 0;
 	static int[][] v;
 	static int[] visited;
 	public static void main(String[] args) throws IOException{
@@ -17,36 +20,33 @@ public class Main{
 		StringTokenizer st;
 		t = Integer.parseInt(br.readLine());
 		
-		v = new int[t+1][t+1];
+		a = new ArrayList[t+1];
 		visited = new int[t+1];
 		int cnt = 0;
 		
 		int n = Integer.parseInt(br.readLine());
-		for(int i=0; i<n; i++) {
+		for(int i=1; i<=t; i++) {
+			a[i] = new ArrayList<Integer>();
+		}
+		
+		for(int i=1; i<=n; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			v[a][b] = 1;
-			v[b][a] = 1;
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			a[u].add(v);
+			a[v].add(u);
 		}
 		
 		go(1);
-		
-		for(int i=1; i<=t; i++) {
-			if(visited[i] == 1) {
-				cnt++;
-			}
-		}
-		cnt--; // 1번 컴퓨터를 제외해야한다.
-		System.out.println(cnt);
+		System.out.println(count);
 	}
 	
 	public static void go(int here) {
-		if(visited[here] == 1) return;
 		visited[here] = 1;
-		for(int i=1; i<=t; i++) {
-			if(visited[i] == 1) continue;
-			if(v[here][i] == 1) go(i);
+		for(int there : a[here]) {
+			if(visited[there] == 1) continue;
+			go(there);
+			count++;
 		}
 	}
 }
