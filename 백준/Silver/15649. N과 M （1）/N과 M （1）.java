7d3610e9ae72d6a6fd487abba5f950a84740e9aa
sgo722 <88089193+sgo722@users.noreply.github.com>
2024-01-20 08:51:03 +0900

    [Silver III] Title: N과 M (1), Time: 388 ms, Memory: 30660 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -13,67 +13,45 @@ import java.util.StringTokenizer;
  * 스트링 객체를 생성하는 과정에서 시간이 많이 걸릴것같아 StringBuilder를 넘겨주는게 좋지 않을까?
  * => 해보려고했으나 StringBuilder객체를 이용하면 출력이 이상하게 나옴.
  * StringBuilder를 이용한 구현 실패 - 08:43
+ * sb를 이용한 풀이 답 참고했음.
+ * 
  */
 
 public class Main{
 	
-	static int n,m;
-	static int[] visited;
+	public static int[] arr;
+	public static int n,m;
+	public static int[] visited;
+	public static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		
+		arr = new int[n+1];
 		visited = new int[n+1];
 		combi(0,"",0);
+		System.out.println(sb);
+		
 	}
 	
 	public static void combi(int idx, String s, int cnt) {
 		if(cnt == m) {
-			System.out.println(s);
+			for(int i=0; i<cnt; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
 			return;
 		}
 		
 		for(int i=1; i<=n; i++) {
 			if(visited[i] == 0) {
 				visited[i] = 1;
+				arr[cnt] = i;
 				combi(i,s+i+" ",cnt+1);
 				visited[i] = 0;
 			}
 		}
 	}
 }
-
-// StringBuilder를 이용한 코드인데 어떤 부분에서 문제가 있는지 모르겠음.
-//public class Main{
-//	
-//	static int n,m;
-//	static int[] visited;
-//	public static void main(String[] args) throws IOException{
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-//		StringBuilder sb = new StringBuilder();
-//		n = Integer.parseInt(st.nextToken());
-//		m = Integer.parseInt(st.nextToken());
-//		
-//		visited = new int[n+1];
-//		combi(0,sb,0);
-//	}
-//	
-//	public static void combi(int idx, StringBuilder sb, int cnt) {
-//		if(cnt == m) {
-//			System.out.println(sb);
-//			return;
-//		}
-//		
-//		for(int i=1; i<=n; i++) {
-//			if(visited[i] == 0) {
-//				visited[i] = 1;
-//				combi(i,sb.append(i+" "),cnt+1);
-//				visited[i] = 0;
-//			}
-//		}
-//	}
-//}
 
