7995b9582f7117c25bc4cfd444d46814aa23a385
sgo722 <88089193+sgo722@users.noreply.github.com>
2024-01-20 22:14:33 +0900

    [Silver III] Title: N과 M (4), Time: 152 ms, Memory: 16260 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -7,32 +7,41 @@ import java.util.StringTokenizer;
  * 아이디어 - 이전 문제들과 비슷해서
  * 그냥 visited생각안하고 사이즈만 맞으면 출력하면 될 것 같은데
  * 이런 경우는 String객체를 만들순있어보이는데, StringBuilder는 어떻게 사용해야할지 모르겠다.
+ * 
+ * 답확인) StringBuilder배열을 가지고 만들수 있다.
  */
 
 public class Main{
+	static int[] arr;
 	static int n, m;
+	static StringBuilder sb;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		sb = new StringBuilder();
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 
-		int[] arr = new int[n+1];
-		for(int i=1; i<=n; i++) {
-			arr[i] = i;
-		}
+		arr = new int[m];
 		
-		go(1, "", 0);
+		go(1, 0);
+		
+		System.out.println(sb);
 	}
 	
-	public static void go(int idx, String s, int size) {
+	public static void go(int idx, int size) {
 		if(size == m) {
-			System.out.println(s);
+			for(int i : arr) {
+				sb.append(i).append(' ');
+			}
+			sb.append("\n");
 			return;
 		}
 		
 		for(int i=idx; i<=n; i++) {
-			go(i,s+i+" ", size+1);
+			arr[size] = i;
+			go(i, size+1);
 		}
 	}
 }
+
