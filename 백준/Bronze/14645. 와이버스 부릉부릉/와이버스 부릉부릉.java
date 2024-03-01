d3929c845b8574d0f208d70824c9e933e894e7a8
sgo722 <88089193+sgo722@users.noreply.github.com>
2024-03-01 23:47:27 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 76 ms, Memory: 11304 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+        }
+
+        System.out.println("비와이");
+
+    }
+}
