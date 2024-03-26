b9c1801ec47f6c1793585eec2435338eed35337b
sgo722 <88089193+sgo722@users.noreply.github.com>
2024-03-27 01:05:53 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 244 ms, Memory: 30280 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,74 +1,64 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 
 public class Main{
-	
-	static class Point{
-		private int y;
-		private int x;
-		public Point(int y, int x) {
-			this.y = y;
-			this.x = x;
-		}
-	}
-	static int n;
-	static final int MAX_N = 987654321;
-	static int[][] a;
-	static int[][] visited;
-	static int[] dy = {-1,0,1,0};
-	static int[] dx = {0,1,0,-1};
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int cnt = 0;
-		while(true) {
-			cnt++;
-			n = Integer.parseInt(br.readLine());
-			if(n == 0) break;
-			a = new int[n][n];
-			visited = new int[n][n];
-			for(int i=0; i<n; i++) {
-				st =  new StringTokenizer(br.readLine(), " ");
-				for(int j=0; j<n; j++) {
-					a[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			for(int i=0; i<n; i++) {
-				for(int j=0; j<n; j++) {
-					visited[i][j] = MAX_N;
-				}
-			}
-			visited[0][0] = a[0][0];
-			go(0,0);
-			sb.append("Problem " + cnt + ": "+ visited[n-1][n-1] + "\n");
-		}
-		System.out.println(sb);
-	}
 
-	static void go(int y, int x){
-		Queue<Point> q = new LinkedList<>();
-		q.add(new Point(y,x));
-		
-		while(!q.isEmpty()) {
-			Point front = q.poll();
-			y = front.y;
-			x = front.x;
-			for(int i=0; i<4; i++) {
-				int ny = y + dy[i];
-				int nx = x + dx[i];
-				if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
-				if(visited[ny][nx] > (visited[y][x] + a[ny][nx])) {
-					visited[ny][nx] = visited[y][x] + a[ny][nx];
-					q.add(new Point(ny,nx));
-				}
-			}
-		}
-	}
+    static class Point {
+        int y;
+        int x;
+
+        public Point(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
+    static int[][] map, visited;
+    static int n;
+
+    static int[] dy = {-1,0,1,0};
+    static int[] dx = {0,1,0,-1};
+    public static void main(String[] args) throws IOException{
+        int test = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        while(true) {
+            test++;
+            n = Integer.parseInt(br.readLine());
+            if(n == 0) break;
+            map = new int[n][n];
+            visited = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                    visited[i][j] = Integer.MAX_VALUE;
+                }
+            }
+
+            bfs();
+            System.out.println("Problem " + test + ": " + visited[n-1][n-1]);
+        }
+    }
+
+    static void bfs(){
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(0,0));
+        visited[0][0] = map[0][0];
+
+        while(q.size() > 0){
+            Point p = q.poll();
+            int y = p.y;
+            int x = p.x;
+
+            for(int i=0; i<4; i++){
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                if(ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
+                if(visited[ny][nx] > visited[y][x] + map[ny][nx]){
+                    visited[ny][nx] = visited[y][x] + map[ny][nx];
+                    q.add(new Point(ny,nx));
+                }
+            }
+        }
+    }
 }
